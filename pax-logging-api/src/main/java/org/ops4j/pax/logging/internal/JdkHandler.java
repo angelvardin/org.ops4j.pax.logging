7777f2b869727ff67450304b352e61ab4984b662
Angel Vardin <a.vardin@seeburger.com>
2021-08-12 10:06:15 +0300

    Fix typos in Javadocs

--- pax-logging-api/src/main/java/org/ops4j/pax/logging/internal/JdkHandler.java
+++ pax-logging-api/src/main/java/org/ops4j/pax/logging/internal/JdkHandler.java
@@ -37,7 +37,7 @@ import org.ops4j.pax.logging.PaxLoggingManager;
  * {@link java.text.MessageFormat#format(String, Object...)} method on log record's message.
  * It doesn't do anything with remaining fields of {@link LogRecord}.
  * <p>
- * Since the backport of TLSv1.3 in java (JDK-8248721) the the logic of TLS debug traces has changed. The TLS log records contain
+ * Since the backport of TLSv1.3 in java (JDK-8248721) the logic of TLS debug traces has changed. The TLS log records contain
  * additional string parameters. They are without a format parameter in the log message. The {@link JdkHandler} appends them
  * to the end of the TLS log message.
  * <p>
